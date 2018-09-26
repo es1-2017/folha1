82ea0ee574e1bc1fde86fe7e6da435f1c476d902
Adriano Lopes <adriano.lopes@iscte-iul.pt>
2018-09-26 23:28:50 +0100

    Nova alteraçao

--- FichaGit/src/fichas/Hello.java
+++ FichaGit/src/fichas/Hello.java
@@ -5,6 +5,10 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello");
+        System.out.println("Hello2");
+        
+        System.out.println("Hello3");
+        
 	}
 
 }
